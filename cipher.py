9a2bfa04ee7c708cfd53526b45f6c63c738332f1
Dorice Smith <doricespieces@gmail.com>
2024-11-07 01:49:21 -0500

    Added Caesar Cipher

--- cipher.py
+++ cipher.py
@@ -1,1 +1,20 @@
-# add your code here
+# Caesar Cipher assignment
+message = input("Please enter a sentence:")
+shift = 5
+result = ""
+#Go through each letter in the message
+for l in message.upper():
+    #Only change letters not punctuation not spaces
+    if l.isalpha():
+        #Assigns number to letter from ASCII
+        letter_key = ord(l)
+        encypted_letter = letter_key + shift
+        #Z is 90 on ASCII but other character follow
+        if encypted_letter > 90:
+            encypted_letter -= 26
+            #convert back to corresponding letter
+        encrypted_sentence = chr(encypted_letter)
+        result = result + encrypted_sentence
+    else:
+        result += l
+print(result)
